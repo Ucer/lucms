cf00e5fa7424a5de7cc99e967eeac090378a55c2
ucer <185429135@qq.com>
2018-11-27 18:55:06 +0800

    cache 机制修改

--- app/Traits/CacheTrait.php
+++ app/Traits/CacheTrait.php
@@ -8,13 +8,25 @@ use Illuminate\Support\Facades\Cache;
 trait CacheTrait
 {
     protected $cacheKeyType = [
-        'smsCode' => 'smsCode_', // 短信验证码 _[18313852226]
+        'smsCode' => [
+            'name' => 'smsCode_', // 短信验证码 _[18313852226]
+            'expiry_time' => 3600 * 5 // 过期时间[秒]，0 为永久有效
+        ],
     ];
 
-    protected function cachePut($cacheKeyType, $key, $value, $expiredAt)
+    protected function cachePut($cacheKeyType, $key, $value, $expireAt)
     {
         $cacheKey = $this->cacheKeyType[$cacheKeyType] . $key;
-        Cache::put($cacheKey, $value, $expiredAt);
+        if ($expireAt) {
+            Cache::put($cacheKey['name'], $value, $expireAt);
+        } else {
+            if ($cacheKey['expiry_time'] > 0) {
+                Cache::put($cacheKey['name'], $value, $cacheKey['expiry_time']);
+            } else {
+                Cache::put($cacheKey['name'], $value);
+            }
+        }
+
         return $cacheKey;
     }
 
